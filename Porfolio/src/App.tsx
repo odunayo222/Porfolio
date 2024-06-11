e9c96cd8de888db2ad1cf83b2890085512e1e3a1
odunayo222 <mayowaakanke@gmail.com>
2024-06-11 18:53:51 +0100

    first commit

---
+++ Porfolio/src/App.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <div className="container border w-[95%] md:max-w-[70%] mx-auto">
+      <div className="w-100 flex">
+        <div className="w-[30%]  bg-blue-600 border h-screen  ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?
+        </div>
+        <div className="w-[70%] border ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          sapiente, fugiat tempora vitae excepturi itaque rem, quibusdam
+          perspiciatis odit unde voluptatem animi, nam voluptatibus at optio
+          quas nobis eius eos?
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
